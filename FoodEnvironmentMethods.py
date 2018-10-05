13584d10da81baf25c0e2efcf22cc9e0b6a113d0
Scheider <s.scheider@uu.nl>
2018-10-05 13:33:55 +0200

    init

---
+++ FoodEnvironmentMethods.py
@@ -0,0 +1,39 @@
+#-------------------------------------------------------------------------------
+# Name:        Methods for capturing the food environment of tracks in terms of
+#               accessible (afforded) food outlets.   This includes identifying
+#               fixed and flexible (food) events in a track, as well as calculating
+#               space-time prisms over a network to capture affordance space.
+# Purpose:       In order to assess the food environment from an affordance perspective
+#
+# Author:      Simon Scheider
+#
+# Created:     05/10/2018
+# Copyright:   (c) Simon Scheider 2018
+# Licence:     MIT license
+#-------------------------------------------------------------------------------
+
+
+
+
+
+"""Function for constructing fixed and flexible (food) events from a collection of trips"""
+def constructEvents():
+    pass
+
+"""Function for computing space-time prism accessibility for a type of (food) event"""
+def getAccessibility(starttime, startpoint, endtime, endpoint, mode, mineventduration=5):
+    prism = None
+    return prism
+
+"""Function for getting accessible outlets, given an accessibility prism"""
+def getAccessibleOutlets(prism):
+    pass
+
+
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
